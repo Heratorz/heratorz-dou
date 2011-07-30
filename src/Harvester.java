b62ad1e0176766404267840dcb6b904843e91b7c
seb <seb@PC-SEB.alfapeople.ua>
2011-07-31 01:53:38 +0300

    full shit

---
+++ src/Harvester.java
@@ -0,0 +1,33 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+/**
+ * Harvester unit's wrapper  
+ */
+public class Harvester extends FlyObject
+{
+	Planet basePlanet;
+	Pt sourcePt = null;
+	Source source = null;
+	BufferedImage bgImage = null;
+	
+   public Harvester(Pt P, int Side, int Size, Planet BasePlanet) {
+      super(P, Side, Size);
+	  
+      basePlanet = BasePlanet;      	   
+      try { bgImage = ImageIO.read(new File("img/harvester.png")); }
+      catch(Exception e) { e.printStackTrace(); }
+   }
+      
+   @Override
+	public void paint(Graphics2D g2) {
+		// TODO Auto-generated method stub
+	   g2.drawImage(bgImage, WC.LX + basePlanet.p.x * WC.SZ, WC.LY + basePlanet.p.y * WC.SZ, size * WC.SZ, size * WC.SZ, null);
+	}
+}
---
+++ src/Utils.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Utils
+{
+   static Random randomizer = new Random(); 
+   
+   public static void randomShuffle(LinkedList<Pt> o) {
+      int n = o.size();
+      for (int i = 0; i < n-1; i++) {
+         int j = i+1+randomizer.nextInt(n-i-1);
+         Pt nxt = o.get(j);
+         o.set(j, o.get(i));
+         o.set(i, nxt);
+      }
+   }
+   
+   // Returns Euclidean object-object distance  
+   public static double getDistance(Pt a, Pt b) {
+      return Math.sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y));
+      //return Math.hypot(a.x-b.x, a.y-b.y);
+   }
+}
