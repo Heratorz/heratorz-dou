8b5f487f99662dbce15571485c6d2406fed13ead
Sergey Olendarenko <sergey@olendarenko.org.ua>
2011-07-31 07:11:15 +0300

    drawing of selection was moved to WorldEnv

---
+++ src/Pt.java
@@ -0,0 +1,9 @@
+/**
+ * Point wrapper  
+ */
+public class Pt
+{
+   int x, y;
+   public Pt(int X, int Y) { x = X; y = Y; }
+   public String toString() { return "Pt("+x+", "+y+")"; }  
+}
---
+++ src/Source.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+/**
+ * Source unit's wrapper  
+ */
+public class Source extends FlyObject
+{
+   public Source(Pt P, int Size, int Id) {
+      super(P, -1, Size, Id);
+   }
+   
+   public void paint(Graphics2D g2) {
+	 Color c = side == 0 ? Color.BLUE : side == 1 ? Color.RED : Color.LIGHT_GRAY;
+	 g2.setColor(c);
+     g2.fillOval(WC.LX+p.x*WC.SZ, WC.LY+p.y*WC.SZ, size*WC.SZ, size*WC.SZ);
+     drawInfo(g2);
+   }
+   
+   public String toString() {
+      return "source";
+   }
+}
