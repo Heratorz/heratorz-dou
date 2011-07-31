ac5707d81dce054e59f9f5db53ade27b8113d1e1
seb <seb@PC-SEB.alfapeople.ua>
2011-07-31 05:46:08 +0300

    Merge

--- src/Planet.java
+++ src/Planet.java
@@ -1,3 +1,9 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Random;
+import javax.imageio.ImageIO;
+
 /**
  * Planet unit's wrapper  
  */
@@ -5,5 +11,19 @@ public class Planet extends FlyObject
 {
    public Planet(Pt P, int Side, int Size) {
       super(P, Side, Size);
+      imageFileName = "img/planet_" + ((new Random()).nextInt(3) + 1) + ".png";
    }
+   
+   @Override
+   public void paint(Graphics2D g2) {
+	   BufferedImage image = null;
+	   try {
+		   image = ImageIO.read(new File(imageFileName));
+	   }
+	   catch (Exception e) { e.printStackTrace(); KissMyAsser.errorFound(); }
+	   g2.drawImage(image, WC.LX+p.x*WC.SZ, WC.LY+p.y*WC.SZ, size*WC.SZ, size*WC.SZ, null);
+	   super.paint(g2);
+   }
+   
+   private String imageFileName;
 }
