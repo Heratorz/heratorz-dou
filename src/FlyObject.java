8a92305574fceb5a4b9f1e76fc5cb02974b24138
seb <seb@PC-SEB.alfapeople.ua>
2011-07-31 05:45:12 +0300

    test

---
+++ src/FlyObject.java
@@ -0,0 +1,41 @@
+import java.awt.Graphics2D;
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * Abstract flying unit's wrapper  
+ */
+abstract class FlyObject
+{
+   Pt p;
+   int side;
+   int size;
+   
+   public FlyObject(Pt P, int Side, int Size) {
+      p = P;
+      side = Side;
+      size = Size;
+   }
+   
+   public void paint(Graphics2D g2) {
+      Color c = side == 0 ? Color.BLUE : side == 1 ? Color.RED : Color.GRAY;
+      g2.setColor(c);
+      g2.drawOval(WC.LX+p.x*WC.SZ, WC.LY+p.y*WC.SZ, size*WC.SZ, size*WC.SZ);
+      g2.drawString("side: " + side + " size: " + size + " corner: " + p.toString() + " center: " + getCenter().toString(), WC.LX+p.x*WC.SZ, WC.LY+p.y*WC.SZ);
+      //System.out.println(p.toString());
+   }
+   
+   public boolean canMove(int Dx, int Dy, int N, int M) {
+      int x2 = p.x+Dx;
+      int y2 = p.y+Dy;
+      return x2 >= 0 && x2 < N && y2 >= 0 && y2 < M;
+   }
+   
+   public void move(Pt P) {
+      p = P;
+   }
+   
+   public Pt getCenter() {
+      return new Pt(p.x+size/2, p.y+size/2);
+   }
+}
---
+++ src/KissMyAsser.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Losers come here...
+ */
+public class KissMyAsser
+{
+   public static void errorFound() {
+      JOptionPane.showMessageDialog(null, "GPF!");
+   }
+}
