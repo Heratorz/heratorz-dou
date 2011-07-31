e4ac485ce31b5959394305d4a1d5295b5a56da71
?? <??@??-??>
2011-07-31 08:48:34 +0300

    ??????????? ???

--- src/Harvester.java
+++ src/Harvester.java
@@ -15,22 +15,22 @@ public class Harvester extends FlyObject
    int cargoWood;
    public static final int priceGold = 5;
    public static final int priceWood = 5;
+   BufferedImage bgImage;
    
    public Harvester(Pt P, int Side, int Id, Planet home) {
       super(P, Side, 4, Id);
       cargoGold = cargoWood = 0;
       targetMine = null;
       targetPlanet = home;
+      try { bgImage = ImageIO.read(new File("img/harvester.png")); }
+      catch(Exception e) { KissMyAsser.errorFound(); }
    }
 
    @Override
 	public void paint(Graphics2D g2) {
-		// TODO Auto-generated method stub
-		//super.paint(g2);
-	   BufferedImage bgImage = null;
-	      try { bgImage = ImageIO.read(new File("img/harvester.png")); }
-	      catch(Exception e) { e.printStackTrace(); }
-	      g2.drawImage(bgImage, WC.LX+p.x*WC.SZ, WC.LY+p.y*WC.SZ, size*WC.SZ, size*WC.SZ, null);
+	   g2.drawImage(bgImage, WC.LX+p.x*WC.SZ, WC.LY+p.y*WC.SZ, size*WC.SZ, size*WC.SZ, null);
+	   drawSelection(g2);
+	   drawInfo(g2);
 	} 
 
    public String toString() {
--- src/Source.java
+++ src/Source.java
@@ -11,11 +11,11 @@ public class Source extends FlyObject
    }
    
    public void paint(Graphics2D g2) {
-	 Color c = side == 0 ? Color.BLUE : side == 1 ? Color.RED : Color.LIGHT_GRAY;
-	 g2.setColor(c);
-     g2.fillOval(WC.LX+p.x*WC.SZ, WC.LY+p.y*WC.SZ, size*WC.SZ, size*WC.SZ);
-     drawSelection(g2);
-     drawInfo(g2);
+	   Color c = side == 0 ? Color.BLUE : side == 1 ? Color.RED : Color.LIGHT_GRAY;
+	   g2.setColor(c);
+      g2.fillOval(WC.LX+p.x*WC.SZ, WC.LY+p.y*WC.SZ, size*WC.SZ, size*WC.SZ);
+      drawSelection(g2);
+      drawInfo(g2);
    }
    
    public String toString() {
