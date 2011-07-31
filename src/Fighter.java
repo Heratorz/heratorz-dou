c6f8ed63d1c12807a58cee500b6f16450e2828ed
Sergey Olendarenko <sergey@olendarenko.org.ua>
2011-07-31 10:38:41 +0300

    HarvestFight

--- src/Fighter.java
+++ src/Fighter.java
@@ -11,11 +11,23 @@ public class Fighter extends FlyObject
    public static final int priceGold = 10;
    public static final int priceWood = 10;
    
+   private BufferedImage bgImage;
+   
    public Fighter(Pt P, int Side, int Id) {
       super(P, Side, 4, Id);
+      bgImage = null;
+      
+      try { bgImage = ImageIO.read(new File("img/fighter" + (Side == 0? "" : Side == 1? "_r" : "_g") + ".png")); }
+      catch (Exception e) { KissMyAsser.errorFound(); }
    }
    
    public String toString() {
       return "fighter";
+   }
+   
+   @Override
+   public void paint(Graphics2D g2) {
+	    g2.drawImage(bgImage, WC.LX+p.x*WC.SZ, WC.LY+p.y*WC.SZ, size*WC.SZ, size*WC.SZ, null);
+        drawInfo(g2);
    }
 }
