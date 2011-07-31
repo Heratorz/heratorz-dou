06647c6b17794883df5a8c2253e508fcbef4512f
Sergey Olendarenko <sergey@olendarenko.org.ua>
2011-07-31 09:23:53 +0300

    wtf?

--- src/Source.java
+++ src/Source.java
@@ -1,6 +1,10 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 
 /**
  * Source unit's wrapper  
@@ -10,16 +14,20 @@ public class Source extends FlyObject
 	SourceType type;
 	int amount = 0;
 	
+	private BufferedImage imageBack;
+	
    public Source(Pt P, int Size, int Id, SourceType Type, int Amount) {
       super(P, -1, Size, Id);
       type = Type;
       amount = Amount;
+      imageBack = null;
+      
+      try { imageBack = ImageIO.read(new File("img/source_" + (Type == SourceType.GOLD? "1" : "2") + ".png")); }
+      catch (Exception e) { KissMyAsser.errorFound(); }
    }
    
    public void paint(Graphics2D g2) {
-	 Color c = side == 0 ? Color.BLUE : side == 1 ? Color.RED : Color.LIGHT_GRAY;
-	 g2.setColor(c);
-     g2.fillOval(WC.LX+p.x*WC.SZ, WC.LY+p.y*WC.SZ, size*WC.SZ, size*WC.SZ);
+     g2.drawImage(imageBack, WC.LX+p.x*WC.SZ, WC.LY+p.y*WC.SZ, size*WC.SZ, size*WC.SZ, null);
      drawInfo(g2);
      if(selected)
      {
